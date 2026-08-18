8dccf799dd1d797876c7026e7591e05db1c29382
fgrim4227 <grimaldofranciscojavier@gmail.com>
2026-08-18 16:23:30 -0400

    Implementation of the Bot
    Based on the base left by R3mmurd, a simple bot was implemented to play pong against

---
+++ 01-pong/src/Bot.py
@@ -0,0 +1,57 @@
+from src.Paddle import Paddle
+from src.Ball import Ball
+import settings
+class Bot(Paddle):
+    def __init__(self, x : float, y : float, width : float, height : float, ball : Ball) -> None:
+        #Parent's constructor method
+        super().__init__(x, y, width, height)
+        self.already_calculated : bool = False
+        self.target_y : float = self.y
+        self.ref_ball : Ball = ball
+
+    def calculate_target(self, dt: float) -> None:
+        if(self.ref_ball.vx > 0):
+            curr_x = self.ref_ball.x
+            curr_y = self.ref_ball.y
+            curr_vx = self.ref_ball.vx
+            curr_vy = self.ref_ball.vy
+            b_height = self.ref_ball.height
+            r_lim = settings.VIRTUAL_WIDTH - self.width - settings.PADDLE_X_OFFSET - self.ref_ball.width
+            lower_lim = settings.VIRTUAL_HEIGHT
+            if not self.already_calculated:
+                while curr_x <= r_lim:
+                    if(curr_y <= 0):
+                        curr_y = 0
+                        curr_vy *= -1
+                    elif(curr_y >= lower_lim):
+                        curr_y = lower_lim - b_height
+                        curr_vy *= -1
+                    print(curr_x)
+                    curr_x += curr_vx * dt
+                    curr_y += curr_vy * dt
+                self.target_y = curr_y
+                self.already_calculated = True
+        else:
+            self.target_y = settings.VIRTUAL_HEIGHT / 2
+            self.already_calculated = True
+
+    def move_towards_goal(self, step : float)->None:
+        if(round(self.y + self.height / 2) > self.target_y and (self.y + self.height / 2) - self.target_y > 2):
+            self.y += -settings.PADDLE_SPEED * step
+        elif(round(self.y + self.height / 2) < self.target_y and self.target_y - (self.y + self.height / 2) > 2):
+            self.y += settings.PADDLE_SPEED * step
+        if(self.y < 0):
+            self.y = 0
+        if(self.y >= settings.VIRTUAL_HEIGHT):
+            self.y = settings.VIRTUAL_HEIGHT - (self.height)
+
+    def update(self, dt: float) -> None:
+            if(self.already_calculated):
+                self.move_towards_goal(dt)
+            else:
+                self.calculate_target(dt)
+                self.already_calculated = True
+        
+
+                    
+
